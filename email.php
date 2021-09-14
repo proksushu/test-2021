b2a585e3872e425788a45ac991e921b6f2d6d453
Oksana <proksushu@yandex.ru>
2021-09-14 19:32:36 +0300

    fix: up html

--- email.php
+++
@@ -1,8 +0,0 @@
-<?php
-$to = "proksushu@yandex.ru";
-  $subject = "Answer for Resume";
-  $message = "Message!";
-  $headers = "Content-type: text/plain; charset=utf-8 \r\n";//Шапка сообщения,
-  //содержит определение типа письма, от кого, и кому отправить ответ на письмо
-  mail ($to, $subject, $message, $headers);
-?>
